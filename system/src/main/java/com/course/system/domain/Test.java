b09278633b4008b62854303651795c332f3a418c
Ricado <haozhengxu@stu.xidian.edu.cn>
2021-06-01 11:10:51 +0800

    1.增加了mybatis使用示例: http://127.0.0.1:9001/system/test 2.增加了controller,service,mapper层
    Signed-off-by: Ricado <haozhengxu@stu.xidian.edu.cn>

---
+++ system/src/main/java/com/course/system/domain/Test.java
@@ -0,0 +1,21 @@
+package com.course.system.domain;
+
+public class Test {
+    private String id;
+    private String name;
+
+    public String getId(){
+        return id;
+    }
+    public void setId(){
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

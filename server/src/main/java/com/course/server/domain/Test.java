2d976857b06300350306f6da1fb9f6814291437d
Ricado <haozhengxu@stu.xidian.edu.cn>
2021-06-08 17:00:10 +0800

    1.将通用的配置移动到server 注:server配置文件的路径要放在resources/config/application.properties,不能和上层的路径一样放在resource根目录下 2.集成了mybatis-generator: 步骤1:添加idea插件(在总pom里添加plugin) 步骤2:添加server\src\main\resources\generator\generatorConfig.xml 步骤3:创建maven启动命令:mybatis-generator:generate -e
    Signed-off-by: Ricado <haozhengxu@stu.xidian.edu.cn>

--- server/src/main/java/com/course/server/domain/Test.java
+++ server/src/main/java/com/course/server/domain/Test.java
@@ -2,12 +2,14 @@ package com.course.server.domain;
 
 public class Test {
     private String id;
+
     private String name;
 
-    public String getId(){
+    public String getId() {
         return id;
     }
-    public void setId(){
+
+    public void setId(String id) {
         this.id = id;
     }
 
@@ -17,5 +19,17 @@ public class Test {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append("]");
+        return sb.toString();
     }
 }
